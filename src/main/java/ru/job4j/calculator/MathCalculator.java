9e35046bff686dff53e3ed97ee8a3bc78ddf3b4a
Melkiy <art.pershin01@yandex.ru>
2023-05-22 15:24:55 +0300

    1.2. Входные данные в тесте [#297750]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,23 +1,23 @@
 package ru.job4j.calculator;
 
-import static ru.job4j.math.MathFunction.*;
+import ru.job4j.math.MathFunction;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second)
-                + multiply(first, second);
+        return MathFunction.sum(first, second)
+                + MathFunction.multiply(first, second);
     }
 
     public static double diffAndDiv(double first, double second) {
-        return diff(first, second)
-        + div(first, second);
+        return MathFunction.diff(first, second)
+        + MathFunction.div(first, second);
     }
 
     public static double plusAll(double first, double second) {
-        return diff(first, second)
-                + div(first, second)
-                + multiply(first, second)
-                + sum(first, second);
+        return MathFunction.diff(first, second)
+                + MathFunction.div(first, second)
+                + MathFunction.multiply(first, second)
+                + MathFunction.sum(first, second);
 
     }
 
